6a156c5dc56b8497bffd9561ef8585c0693d9247
Nguyen Thanh Phat <75824684+nguyenthanhphatqn@users.noreply.github.com>
2022-11-04 00:34:59 +0700

    done

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // swcMinify: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     domains: ["scontent.fsgn5-12.fna.fbcdn.net", "lh3.googleusercontent.com"],
   },
